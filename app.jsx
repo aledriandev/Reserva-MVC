5eb9da1115c9a7e2d3bf293a484cd46a89a27914
AQP20171679 <alejandraadriantejada@gmail.com>
2017-10-04 12:02:54 -0500

    Remueve agrega y cuenta confirmados

--- app.jsx
+++ app.jsx
@@ -1,30 +1,79 @@
 class Model {
   constructor () {
-    this.invited = {
-      name: '',
-      confirm: false,
-    };
     this.invitedList = [{
       name: 'Ale',
+      confirm: false,
+      id: 0,
+      class: ''
     }];
+    this.invitedConfirm = [];
+    this.invited = '';
     this.callback = null;
+    this.id = 1;
   }
   
   subscribe(render) {
     this.callback = render;
   }
 
-}
+  addInvited () {
+    this.invitedList.push({
+      name: this.invited,
+      confirm: false,
+      id: this.id,
+      class: '',
+    });
+    this.id = this.id + 1;
+    this.invited = '';
+    this.callback();
+  }
+
+  onChange (e) {
+    console.log(e.target.value)
+    this.invited = e.target.value;
+    this.callback();
+  }
+
+  onSubmit (e) {
+    e.preventDefault();
+    this.addInvited();
+  }
+
+  cheCk (e, index) {
+    // let index = parseInt(e.target.parentNode.id);
+    if (this.invitedList[index].confirm) {
+      this.invitedList[index].confirm = false;
+      this.invitedList[index].class = '';
+      this.invitedConfirm.pop();
+    } else {
+      this.invitedList[index].confirm = true;
+      this.invitedList[index].class = 'responded';
+      this.invitedConfirm.push(index);
+    }
+    console.log(this.invitedConfirm.length);
+    this.callback();
+  }
+
+  remove (e,index) {
+    // console.log(e.target.parentNode.id);
+    // let index = parseInt(e.target.parentNode.id);
+    this.invitedList.splice(index,1);
+    this.callback();
+  }
+  
+
+} 
+  
 
 const Rsvp = ({model}) => {
   const getList = () => {
     return model.invitedList.map((invited,index) => {
-      return (<li key={index}>
-          {invited.name}
-          <label >Confirmed</label>
-          <input type="checkbox" name id />
-          <button>Remove</button>
-      </li>);
+      return (<li key={index} id={invited.id} className={invited.class}>
+                {invited.name}
+                <br/>
+                <label>Confirmed</label><input type="checkbox" checked={invited.confirm} onChange={e=>{model.cheCk(e,index)}}/><br/>
+                <button onClick={e=>{model.remove(e,index)}}>Remove</button>
+            </li>);
     });
   }
   return (
@@ -32,8 +81,8 @@ const Rsvp = ({model}) => {
         <header>
           <h1>RSVP</h1>
           <p> Registration App </p>
-          <form id="registrar">
-            <input type="text" name="name" placeholder="Invite Someone" />
+          <form id="registrar" onSubmit={e => {model.onSubmit(e)}}>
+            <input type="text" name="name" placeholder="Invite Someone"  value={model.invited} onChange={e => {model.onChange(e)}}/>
             <button type="submit" name="submit" value="submit">Submit</button>
           </form>
         </header>
